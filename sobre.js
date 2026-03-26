ec518bc5b0f001eb2db0168772dbf5e6bbf6157e
AuxerreAuccelio <auxerreauccelio9@gmail.com>
2026-03-25 22:15:15 -0300

    refatorar_páginas

--- sobre.js
+++ sobre.js
@@ -3,7 +3,7 @@ const botao = document.getElementById("bt-voltar");
 if (botao) {
   botao.addEventListener("click", function (event) {
     event.preventDefault();
-    window.location.href = "index.html";
+    window.location.href = "produtos.html";
   });
 } else {
   alert("Deu erro!");
